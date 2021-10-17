5bdb79ea6fd8c562228b4730674f2bfe76ec4509
0andme <yu00mi97@gmail.com>
2021-10-18 03:06:15 +0900

    fix : 글 목록의 기본값을 닫는 것으로 변경
    기존의 js는 직접 스타일을 바꾸었는데
    이번엔 클래스를 넣고 뺴고 해당 클래스가 있을때와 없을때 css구분하여 작성함

--- assets/js/postListBtn.js
+++ assets/js/postListBtn.js
@@ -1,14 +1,17 @@
 const postlist_btn = document.querySelector(".postListBtn");
 const ulList = document.querySelector("ul.table-of-contents-list");
-postlist_btn.style.cursor="pointer";
+postlist_btn.style.cursor = "pointer";
 postlist_btn.addEventListener('click', function () {
 
-    if (ulList.style.display === "none") {
-        ulList.style.display = "block";
-        postlist_btn.textContent="글 목록 닫기";
+    if (ulList.classList.contains("visual")) {
+
+        ulList.classList.remove("visual");
+        postlist_btn.textContent = "글 목록 펼치기";
+
     } else {
-        ulList.style.display = "none";
-        postlist_btn.textContent="글 목록 펼치기";
+        ulList.classList.add("visual");
+        postlist_btn.textContent = "글 목록 닫기";
 
     }
+
 })
